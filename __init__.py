0ac94048d55e4d3b92c7b215b207c3ca24e4e9ef
fbourgeoi <francois.bourgeois@aiesh.be>
2022-05-25 11:51:10 +0200

    Si problème d'import des librairies, on essaie de les installer

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-from postgresql_lib.postgresql_lib import *
+import os
+# Si problème d'import des librairies, on essaie de les installer
+try:
+	from postgresql_lib.postgresql_lib import *
+except:
+	
+	os.popen(f"pip install --no-cache-dir -r {os.path.dirname(os.path.realpath(__file__))}/requirements.txt").read()
+	from postgresql_lib.postgresql_lib import *
