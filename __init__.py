c9773b3634b2567f4a9f8f1dcb01f32bb1ef1c79
fbourgeoi <francois.bourgeois@aiesh.be>
2022-09-07 14:34:23 +0200

    On n'importe que ce qui est utile

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 import os
 # Si problème d'import des librairies, on essaie de les installer
 try:
-	from postgresql_lib.postgresql_lib import *
+	from postgresql_lib.postgresql_lib import postgresql_database
 except:
 	
 	os.popen(f"pip install --no-cache-dir -r {os.path.dirname(os.path.realpath(__file__))}/requirements.txt").read()
-	from postgresql_lib.postgresql_lib import *
+	from postgresql_lib.postgresql_lib import postgresql_database
